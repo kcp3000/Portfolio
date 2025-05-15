a65cd685a8770645ded3ff1e48c1a7d1c4cbc364
kevin deleon <mrkev63@gmail.com>
2025-05-14 20:56:12 -0400

    finished media

--- portfolio-react/src/sections/section2.jsx
+++ portfolio-react/src/sections/section2.jsx
@@ -13,71 +13,80 @@ export function Section2() {
       <section className="grid_container_section2_text">
         <div className="line"/>
         <h1>BIO</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+        <p>Hello! I'm a:</p> 
+        <p className="bold">FULLSTACK DEVELOPER.</p>
+        <p> 
+          I'm well versed in both Frontend and Backend; primarily using Javascript/Typescript and PostgresSQL
+          as my main tech stack, but alway open to learning more! Most importantly, I love to create interesting, off-the-wall designs that leaves a lasting impression.
+          Ultimately, I'm here to 
         </p>
+        <p className="bold">CREATE.</p> 
+       
       </section>
-      <div className="ticker">
-        <ul className="ticker_list_images">
-          <li>
-            <img src={JS} alt="javascript icon" className="java"/>
-          </li>
-          <li>
-            <img src={TS} alt="typescript icon" className="type"/>
-          </li>
-          <li>
-            <img src={express} alt="express icon" className="exp"/>
-          </li>
-          <li>
-            <img src={framer} alt="motion icon" className="frame"/>
-          </li>
-          <li>
-            <img src={git} alt="git icon" className="git"/>
-          </li>
-          <li>
-            <img src={node} alt="node icon" className="node"/>
-          </li>
-          <li>
-            <img src={knex} alt="knex icon" className="knex"/>
-          </li>
-          <li>
-            <img src={postgres} alt="postgres icon" className="post"/>
-          </li>
-          <li>
-            <img src={reacticon} alt="react icon" className="react"/>
-          </li>
-        </ul>
+      <div className="ticker_container">
+          <div className="ticker">
+          <ul className="ticker_list_images">
+            <li>
+              <img src={JS} alt="javascript icon" className="java"/>
+            </li>
+            <li>
+              <img src={TS} alt="typescript icon" className="type"/>
+            </li>
+            <li>
+              <img src={express} alt="express icon" className="exp"/>
+            </li>
+            <li>
+              <img src={framer} alt="motion icon" className="frame"/>
+            </li>
+            <li>
+              <img src={git} alt="git icon" className="git"/>
+            </li>
+            <li>
+              <img src={node} alt="node icon" className="node"/>
+            </li>
+            <li>
+              <img src={knex} alt="knex icon" className="knex"/>
+            </li>
+            <li>
+              <img src={postgres} alt="postgres icon" className="post"/>
+            </li>
+            <li>
+              <img src={reacticon} alt="react icon" className="react"/>
+            </li>
+          </ul>
 
-        <ul className="ticker_list_images" aria-hidden="true">
-          <li>
-            <img src={JS} alt="javascript icon" className="java"/>
-          </li>
-          <li>
-            <img src={TS} alt="typescript icon" className="type"/>
-          </li>
-          <li>
-            <img src={express} alt="express icon" className="exp"/>
-          </li>
-          <li>
-            <img src={framer} alt="motion icon" className="frame"/>
-          </li>
-          <li>
-            <img src={git} alt="git icon" className="git"/>
-          </li>
-          <li>
-            <img src={node} alt="node icon" className="node"/>
-          </li>
-          <li>
-            <img src={knex} alt="knex icon" className="knex"/>
-          </li>
-          <li>
-            <img src={postgres} alt="postgres icon" className="post"/>
-          </li>
-          <li>
-            <img src={reacticon} alt="react icon" className="react"/>
-          </li>
-        </ul>
+          <ul className="ticker_list_images" aria-hidden="true">
+            <li>
+              <img src={JS} alt="javascript icon" className="java"/>
+            </li>
+            <li>
+              <img src={TS} alt="typescript icon" className="type"/>
+            </li>
+            <li>
+              <img src={express} alt="express icon" className="exp"/>
+            </li>
+            <li>
+              <img src={framer} alt="motion icon" className="frame"/>
+            </li>
+            <li>
+              <img src={git} alt="git icon" className="git"/>
+            </li>
+            <li>
+              <img src={node} alt="node icon" className="node"/>
+            </li>
+            <li>
+              <img src={knex} alt="knex icon" className="knex"/>
+            </li>
+            <li>
+              <img src={postgres} alt="postgres icon" className="post"/>
+            </li>
+            <li>
+              <img src={reacticon} alt="react icon" className="react"/>
+            </li>
+          </ul>
+        </div>
       </div>
+      
     </div>
   
 }
