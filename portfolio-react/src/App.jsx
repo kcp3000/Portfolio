dc63f3defa1d375576ee9d4d375ed88605d4cb81
Kevin <mrkev63@gmail.com>
2025-09-01 21:50:06 -0400

    trying to take out slow

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -1,25 +1,24 @@
 import { Section1 } from "./sections/section1"
 import { Section2 } from "./sections/section2"
 import { Section3 } from "./sections/section3"
-import SmoothScroll from "./components/Smooth_Scroll"
+// import SmoothScroll from "./components/Smooth_Scroll"
 import NavBar from "./components/NavBar"
 import FadeOut from "./components/FadeOut"
 
 
 function App() {
 
-  
+
 
   return (
-  <div>
-    <SmoothScroll>
-        <NavBar />
-        <main>
-          <FadeOut y={0.35} y2={0.45}>
-            <div className="section1">
-              <Section1 />
-            </div>
-          </FadeOut> 
+    <div>
+      <NavBar />
+      <main>
+        <FadeOut y={0.35} y2={0.45}>
+          <div className="section1">
+            <Section1 />
+          </div>
+        </FadeOut>
         <div className="section2">
           <Section2 />
         </div>
@@ -27,8 +26,7 @@ function App() {
           <Section3 />
         </div>
       </main>
-  </SmoothScroll>
-  </div>
+    </div>
 
   )
 }
