41ce5eefd46f30194111fc86938dddbcedcf8095
Kevin <mrkev63@gmail.com>
2025-09-06 14:51:48 -0400

    added wasteWise back

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./pages/Home"
 import Y2K from "./pages/Y2K"
-// import WasteWise from "./pages/WasteWise"
+import WasteWise from "./pages/WasteWise"
 // import CAW from "./pages/CAW"
 import PROJECTS from "./pages/PROJECTS"
 import NotFoundPage from "./pages/NotFoundPage"
@@ -20,7 +20,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/Y2K' element={<Y2K />} />
-      {/* <Route path='/WasteWise' element={<WasteWise />} /> */}
+      <Route path='/WasteWise' element={<WasteWise />} />
       {/* <Route path='/CAW' element={<CAW />} /> */}
       <Route path='/PROJECTS' element={<PROJECTS />} />
       <Route path='*' element={<NotFoundPage />} />
