f06cddae1a2fe88cdf47e5e71df7f8738b04a452
kcp3000 <mrkev63@gmail.com>
2026-02-24 17:33:16 -0500

    finished mobile view for projects

--- portfolio-react/src/pages/PROJECTS.jsx
+++ portfolio-react/src/pages/PROJECTS.jsx
@@ -10,6 +10,7 @@ import waste1 from "../images/wasteWise/README.png"
 import y2k1 from "../images/y2k/y2k1.png"
 import Phx from "../images/Phx.png"
 import gallery from "../images/gallery.png"
+import { useState } from "react"
 
 //https://waste-wise.onrender.com
 //https://ascentak.github.io/Project-Ascent/
@@ -18,17 +19,144 @@ import gallery from "../images/gallery.png"
 //https://www.creativeartworks.org/blogpreview/2025/8/30/a-fusion-of-art-and-information
 
 const PROJECT = () => {
+
+  const [isChanged, setIsChanged] = useState(true);
+
+    const toggleChange = () => {
+        setIsChanged((isChanged) => !isChanged);
+    }
+
   return <>
     <div className="projects_title">
       <SlideAnimateY delay={1} yH={50}>
         <h1 className="Pro_page">PROJECTS</h1>
       </SlideAnimateY>
       <SlideAnimateY delay={1.5} yH={50}>
-        <p>kindly take a look</p>
+        <p className="subtitleProject">kindly take a look</p>
       </SlideAnimateY>
     </div>
-    <section className="project_container">
+{/* FOR MOBILE VIEW BELOW */}
+    <div className="mobileview_projects">
+      <SlideAnimateY delay={2.5} yH={50}>
+      <p className="switch">PRESS TO SWITCH</p>
+      </SlideAnimateY>
+      <SlideAnimateY delay={2} yH={50}>
+        <button className="projectSwitch" onClick={toggleChange}>{isChanged ? "SWE" : "DESIGN"}</button>
+      </SlideAnimateY>
+      {isChanged ? (
+        <div className="SWEMobile_container">
+          <div className="inner_child">
+          <InView d={"grid"} delay={2}>
+          <p className="title_caption">SWE Fullstack --WasteWise</p>
+          <div className="testblock">
+            <img className="waste" src={waste1} alt="screenshot_of_wastewise" />
+          </div>
+        </InView>
+        <InView d={"grid"} delay={3}>
+          <div className="Smalltestblock">
+            <p className="projectMainText">
+              Bridging the gap between food insecurity and food waste by redistributing food that is still perfectly edible but would otherwise be thrown away.
+              Through our platform, we connect New Yorkers facing hunger with local food banks and meal programs, offering resource locators and nutritional guidance.
+              We also promote sustainability by educating the public and showcasing volunteer opportunities, while providing restaurants and companies a simplified food donation network to ensure surplus food reaches people in need.
+              All in all, we aim to build a more sustainable and fair food system for our city.
+            </p>
+            <a href="https://waste-wise.onrender.com" className="Project_link" target="blank_">SITE</a>
+          </div>
+        </InView>
+        </div>
+        <div className="inner_child">
+          <InView d={"grid"} delay={0.5}>
+          <p className="title_caption">SWE Frontend --Acscent</p>
+          <div className="testblock">
+            <img className="phx" src={Phx} alt="pic_of_Phenoix" />
+          </div>
+        </InView>
+        <InView d={"grid"} delay={1}>
+          <div className="Smalltestblock">
+            <p className="projectMainText">
+              Created an application on the hit game VALORANT. This site gives an overview of what the game is about,
+              as well as a catalog of all of the Agents and what their abilities are!
+            </p>
+            <a href="https://ascentak.github.io/Project-Ascent/" className="Project_link" target="blank_">SITE</a>
+          </div>
+        </InView>
+        </div>
+        <div className="inner_child">
+          <InView d={"grid"} delay={0.5}>
+          <p className="title_caption">SWE Frontend --Y2K</p>
+          <div className="testblock">
+            <img className="y2k" src={y2k1} alt="y2k_screenshot" />
+          </div>
+        </InView>
+        <InView d={"grid"} delay={1}>
+          <div className="Smalltestblock">
+            <p className="projectMainText">
+              The start of my coding journey, this website was more of a practice run for my CSS and HTML skills back when I started to really
+              get into coding. It consist of things that I find interesting utilizing the Y2K's ERA aesthetic.
+            </p>
+            <a href="https://kcp3000.github.io/Y2K/" className="Project_link" target="blank_">SITE</a>
+          </div>
+        </InView>
+        </div>
+        <div className="inner_child">
+          <InView d={"grid"} delay={0.5}>
+          <p className="title_caption">SWE Fullstack --GALLERYWALK</p>
+          <div className="testblock">
+            <img className="gallery" src={gallery} alt="screenshot_of_gallery" />
+          </div>
+        </InView>
+        <InView d={"grid"} delay={1}>
+          <div className="Smalltestblock">
+              <p className="projectMainText">
+                To combat against the sea of AI art, here are the classics. 
+                Check out the site (Not in publication currently) and choose from thousands of classical art created BY humans!
+              </p>
+              <a href="https://github.com/Gallery-Walk/Collection-Area" className="Project_link" target="blank_">GITHUB</a>
+          </div>
+        </InView>
+        </div>
+        </div>
+      ) : (
+        <div className="designMobile_container">
 
+          <div>
+            <div className="inner_child">
+              <InView d={"grid"} delay={1}>
+                <p className="title_caption">Design Work --CAW</p>
+                <div className="testblock">
+                  <img className="CAW" src={caw1} alt="pictures_of_booklet" />
+                </div>
+              </InView> 
+              <InView d={"grid"} delay={1.5}>
+                <div className="Smalltestblock">
+                  <p className="projectMainText">
+                    Interned @ CAW (Creative Art Works) for the opportunity to transform the Family Court House booklet for all family courts in NYC!
+                    In the span of ~5 weeks, I along with 17 other talented Young Apprentices using Adobe Indesign worked together to create a booklet worth remembering.
+                    </p>
+                    <a className="Project_link" href="https://www.creativeartworks.org/blogpreview/2025/8/30/a-fusion-of-art-and-information" target="blank_">SITE</a>
+                </div>
+              </InView>
+            </div>
+          </div>
+
+          <div className="inner_child">
+            <InView d={"grid"} delay={0.5}>
+              <p className="title_caption">--</p>
+              <div className="testblock">
+                
+              </div>
+            </InView>
+            <InView d={"grid"} delay={2}>
+              <div className="Smalltestblock">
+                <p className="projectMainText">--</p>
+              </div>
+            </InView>
+          </div>
+        </div>
+      )}
+    </div>
+{/* FOR MOBILE VIEW ABOVE */}
+    <section className="project_container">
       <div className="design_container">
         <SlideAnimateY delay={1.5} yH={50}>
           <h1 className="inner_text">DESIGN</h1>
@@ -151,6 +279,10 @@ const PROJECT = () => {
         </div>
       </div>
     </section>
+
+    
+
+
   </>
 }
 
